cd6615381f1adbd09444a64d051b678f588a0be4
Nico Lutz <nico@bakkenbaeck.no>
2019-08-28 10:44:38 +0200

    Update calculate_bleu_dev.sh

--- webnlg_eval_scripts/calculate_bleu_dev.sh
+++ webnlg_eval_scripts/calculate_bleu_dev.sh
@@ -5,11 +5,10 @@
 export TEST_TARGETS_REF0=test-all-notdelex-reference0.lex
 export TEST_TARGETS_REF1=test-all-notdelex-reference1.lex
 export TEST_TARGETS_REF2=test-all-notdelex-reference2.lex
-# export TEST_TARGETS_REF3=../data/webnlg/all-notdelex-reference3.lex
-# export TEST_TARGETS_REF4=../data/webnlg/all-notdelex-reference4.lex
-# export TEST_TARGETS_REF5=../data/webnlg/all-notdelex-reference5.lex
-# export TEST_TARGETS_REF6=../data/webnlg/all-notdelex-reference6.lex
-# export TEST_TARGETS_REF7=../data/webnlg/all-notdelex-reference7.lex
+export TEST_TARGETS_REF3=test-all-notdelex-reference3.lex
+export TEST_TARGETS_REF4=test-all-notdelex-reference4.lex
+export TEST_TARGETS_REF5=test-all-notdelex-reference5.lex
+export TEST_TARGETS_REF6=test-all-notdelex-reference6.lex
+export TEST_TARGETS_REF7=test-all-notdelex-reference7.lex
 
-../../webnlg_eval_scripts/multi-bleu.perl ${TEST_TARGETS_REF0} ${TEST_TARGETS_REF1} ${TEST_TARGETS_REF2} < relexicalised_predictions.txt
-
+../../webnlg_eval_scripts/multi-bleu.perl ${TEST_TARGETS_REF0} ${TEST_TARGETS_REF1} ${TEST_TARGETS_REF2} ${TEST_TARGETS_REF3} ${TEST_TARGETS_REF4} ${TEST_TARGETS_REF5} ${TEST_TARGETS_REF6} ${TEST_TARGETS_REF7} < relexicalised_predictions.txt
