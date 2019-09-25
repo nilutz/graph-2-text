bc541154ac81d182dade09fb19a1e3019368c561
Nico Lutz <nico@bakkenbaeck.no>
2019-09-25 13:16:11 +0200

    metric

--- cmds/football/notdelex_attr_postfix_1.sh
+++ cmds/football/notdelex_attr_postfix_1.sh
@@ -15,25 +15,25 @@ python3 ../../translate.py -model ../../data/football_${type}_${num}_ctx*e30.pt 
 #python ../../football_processing/relex.py -t ../../data/data-football/${type}_ -p delexicalized_predictions_test_${num}.txt -r test-data-football-gcn-${type}.relex
 
 #translate fake
-python3 ../../translate.py -model ../../data/football_${type}_${num}_ctx*e29.pt -data_type gcn -src ../../data/data-football/${type}_/test_fake-data-football-gcn-${type}-src-nodes.txt -tgt ../../data/data-football/${type}_/test_fake-data-football-gcn-${type}-tgt.txt -src_label ../../data/data-football/${type}_/test_fake-data-football-gcn-${type}-src-labels.txt -src_node1 ../../data/data-football/${type}_/test_fake-data-football-gcn-${type}-src-node1.txt -src_node2 ../../data/data-football/${type}_/test_fake-data-football-gcn-${type}-src-node2.txt -src_ctx ../../data/data-football/${type}_/test_fake-data-football-gcn-${type}-context.txt -output ../../data/data-football/${type}_/delexicalized_predictions_test_fake_${num}.txt -context -replace_unk
+python3 ../../translate.py -model ../../data/football_${type}_${num}_ctx*e30.pt -data_type gcn -src ../../data/data-football/${type}_/test_fake-data-football-gcn-${type}-src-nodes.txt -tgt ../../data/data-football/${type}_/test_fake-data-football-gcn-${type}-tgt.txt -src_label ../../data/data-football/${type}_/test_fake-data-football-gcn-${type}-src-labels.txt -src_node1 ../../data/data-football/${type}_/test_fake-data-football-gcn-${type}-src-node1.txt -src_node2 ../../data/data-football/${type}_/test_fake-data-football-gcn-${type}-src-node2.txt -src_ctx ../../data/data-football/${type}_/test_fake-data-football-gcn-${type}-context.txt -output ../../data/data-football/${type}_/delexicalized_predictions_test_fake_${num}.txt -context -replace_unk
 
 #relex fake
 #python3 ../../football_processing/relex.py -t ../../data/data-football/${type}_ -p delexicalized_predictions_test_fake_${num}.txt -r test-data-football-gcn-${type}.relex
 
 #bleu
-sh ../../football_processing/calculate_bleu.sh ../../data/data-football/${type}_/test-data-football-gcn-${type}-tgt.txt  ../../data/data-football/${type}_/relexicalised_predictions_test_${num}.txt > out_bleu_${type}_${num}.txt
+sh ../../football_processing/calculate_bleu.sh ../../data/data-football/${type}_/test-data-football-gcn-${type}-tgt.txt  ../../data/data-football/${type}_/delexicalized_predictions_test_${num}.txt > out_bleu_${type}_${num}.txt
 
 #conversion for ter and meteor
-python ../../football_processing/metrics.py -t ../../data/data-football/${type}_/ -p relexicalised_predictions_test_${num}.txt -r test-data-football-gcn-${type}-tgt.txt
+python ../../football_processing/metrics.py -t ../../data/data-football/${type}_/ -p delexicalized_predictions_test_${num}.txt -r test-data-football-gcn-${type}-tgt.txt
 
 #ctxe
-python3 ../../football_processing/ctx_eval.py -p ../../data/data-football/${type}_/relexicalised_predictions_fake_${num}.txt -r ../../data/data-football/${type}_/test-data-football-gcn-${type}-tgt.txt -o ../../data/data-football/${type}_/ctx_eval_${num}.txt -f ../../data/data-football/${type}_/relexicalised_predictions_fake_${num}.txt -c ../../data/data-football/${type}_/test-data-football-gcn-${type}-context.txt -x ../../data/data-football/${type}_/test_fake-data-football-gcn-${type}-context.txt
+python3 ../../football_processing/ctx_eval.py -p ../../data/data-football/${type}_/delexicalized_predictions_fake_${num}.txt -r ../../data/data-football/${type}_/test-data-football-gcn-${type}-tgt.txt -o ../../data/data-football/${type}_/ctx_eval_${num}.txt -f ../../data/data-football/${type}_/relexicalised_predictions_fake_${num}.txt -c ../../data/data-football/${type}_/test-data-football-gcn-${type}-context.txt -x ../../data/data-football/${type}_/test_fake-data-football-gcn-${type}-context.txt
 
 #TER produces files out_ter_{type}_{num}.txt with TER data
-java -jar ../../eval_tools/tercom-master/tercom-0.10.0.jar -h ../../data/data-football/${type}_/relexicalised_predictions_test_${num}-ter.txt -r ../../data/data-football/${type}_/test_${num}-all-notdelex-refs-ter.txt > out_ter_${type}_${num}.txt
+java -jar ../../eval_tools/tercom-master/tercom-0.10.0.jar -h ../../data/data-football/${type}_/delexicalized_predictions_test_${num}-ter.txt -r ../../data/data-football/${type}_/test_${num}-all-notdelex-refs-ter.txt > out_ter_${type}_${num}.txt
 
 #METEOR
-java -Xmx2G -jar ../../eval_tools/meteor-master/meteor-1.6.jar ../../data/data-football/${type}_/relexicalised_predictions_test_${num}.txt ../../data/data-football/${type}_/test_${num}-all-notdelex-refs-meteor.txt -r 8 -l de -norm > out_meteor_${type}_${num}.txt
+java -Xmx2G -jar ../../eval_tools/meteor-master/meteor-1.6.jar ../../data/data-football/${type}_/delexicalized_predictions_test_${num}.txt ../../data/data-football/${type}_/test_${num}-all-notdelex-refs-meteor.txt -r 8 -l de -norm > out_meteor_${type}_${num}.txt
 
 
 done
